94f93e58e0ecad1ca3bf9e5bd3f681b11a01f4d0
Jason Allen <jasonallenphotography@gmail.com>
2016-11-12 16:27:22 -0500

    Update routes to reflect root to Events#Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :events
-  resources :users, only: [:new, :create]
+  resources :users
 
+  get '/'=> 'events#index'
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
